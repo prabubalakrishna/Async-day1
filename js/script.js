33b33f7b11853c341017a382ded7052a6eac14cb
prabubalakrishna <prabusingapore89@gmail.com>
2024-05-09 07:58:47 +0530

    Happy Indepence Day

---
+++ js/script.js
@@ -0,0 +1,38 @@
+ setTimeout(()=>{
+    
+    
+    setTimeout(()=>{
+      document.getElementById("countown").innerHTML=10;
+        setTimeout(()=>{
+            document.getElementById("countown").innerHTML=9;
+            setTimeout(()=>{
+               document.getElementById("countown").innerHTML=8;
+                setTimeout(()=>{
+                 document.getElementById("countown").innerHTML=7;
+                    setTimeout(()=>{
+                     document.getElementById("countown").innerHTML=6;
+                        setTimeout(()=>{
+                         document.getElementById("countown").innerHTML=5;
+                            setTimeout(()=>{
+                             document.getElementById("countown").innerHTML=4;
+                                setTimeout(()=>{
+                                 document.getElementById("countown").innerHTML=3;
+                                    setTimeout(()=>{
+                                     document.getElementById("countown").innerHTML=2;
+                                        setTimeout(()=>{
+                                         document.getElementById("countown").innerHTML=1;
+                                            setTimeout(()=>{
+                                             document.getElementById("countown").innerHTML= "Happy Independence Day";
+                                            },1000);
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+
+},1000);
